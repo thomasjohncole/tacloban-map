44a8e833403febd2b79b4e5793caea5c73a244a7
Tom Cole <colecode.ph@gmail.com>
2018-04-05 15:30:49 +0800

    Change single quotes to double quotes as per JSLint

--- js/script.js
+++ js/script.js
@@ -4,40 +4,40 @@ and the Udacity KnockoutJS classes */
 // Model: this is the data used to create the markers and list items
 const locations = [
         {
-            name: 'Serenitea',
+            name: "Serenitea",
             position: {lat: 11.221357, lng: 125.003836},
-            type: 'Real Street',
-            venueID: '57ac5646498ea62681e9636d'
+            type: "Real Street",
+            venueID: "57ac5646498ea62681e9636d"
         },
         {
-            name: 'Skye coffee+wine',
+            name: "Skye coffee+wine",
             position: {lat: 11.237465, lng: 125.002999},
-            type: 'Real Street',
-            venueID: '5777e920498ecedd942ed04d'
+            type: "Real Street",
+            venueID: "5777e920498ecedd942ed04d"
         },
         {
-            name: 'Jose Karlos Coffee Shop',
+            name: "Jose Karlos Coffee Shop",
             position: {lat: 11.241782, lng: 125.005242},
-            type: 'Downtown',
-            venueID: '5823d7c4d1efa6541ad2ae30'
+            type: "Downtown",
+            venueID: "5823d7c4d1efa6541ad2ae30"
         },
         {
-            name: 'Rovinare',
+            name: "Rovinare",
             position: {lat: 11.207398, lng: 125.018457},
-            type: 'Uptown',
-            venueID: '58241574b04e981d14efa4cc'
+            type: "Uptown",
+            venueID: "58241574b04e981d14efa4cc"
         },
         {
-            name: 'Cafe Lucia',
+            name: "Cafe Lucia",
             position: {lat: 11.218591, lng: 125.006297},
-            type: 'Real Street',
-            venueID: '546c72b4498ebfe8af97db76'
+            type: "Real Street",
+            venueID: "546c72b4498ebfe8af97db76"
         },
         {
-            name: 'ABCD Cafe',
+            name: "ABCD Cafe",
             position: {lat: 11.244335, lng: 125.002798},
-            type: 'Downtown',
-            venueID: '51c2ebe4498ea4fe1cf353f8'
+            type: "Downtown",
+            venueID: "51c2ebe4498ea4fe1cf353f8"
         }
     ];
 
@@ -49,7 +49,7 @@ const fsClientSecret = "&client_secret=PIGR2CGDNIJZXV4MXGUHLJ1TMZCJ3NBYFXKLZBFCG
 
 
 // filters to use for dropdown menu
-const filters = ['All', 'Downtown', 'Real Street', 'Uptown'];
+const filters = ["All", "Downtown", "Real Street", "Uptown"];
 
 // sort the locations alpha by name
 locations.sort(function(a, b) {
@@ -63,7 +63,7 @@ locations.sort(function(a, b) {
 
 function initMap() {
     /* Create an object which contains the map options: center, zoom, and styles.
-    Map styles are 'klapsons purple' by Vanlop Ninkhuha taken from snazzymaps.com */
+    Map styles are "klapsons purple" by Vanlop Ninkhuha taken from snazzymaps.com */
     const mapOptions = {
         center: {lat: 11.223399, lng: 125.001354},
         zoom: 14,
@@ -178,7 +178,7 @@ function initMap() {
 
     /* Constructor creates a new map and takes the div element and the
     mapOptions object as arguments */
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     let largeInfowindow = new google.maps.InfoWindow();
     let bounds = new google.maps.LatLngBounds();
@@ -187,29 +187,29 @@ function initMap() {
     function populateInfoWindow(marker, infowindow) {
         // infowindow.marker = marker; // what does this do? do I need it?
         if (marker.fsAddress) {
-            address = '';
+            address = "";
             // The formattedAddress returned from the API request is an array,
             // so we can loop through it and add line breaks for display
             marker.fsAddress.forEach(function (line){
-                address += line + '</br>';
+                address += line + "</br>";
             });
 
-            infowindow.setContent('<div>' +
-                                  '<strong>' + marker.fsName + '</strong>' +
-                                  '</br>' + address +
-                                  '</br>(Address provided by Foursquare)' +
-                                  '</br><a href="http://foursquare.com/v/' +
+            infowindow.setContent("<div>" +
+                                  "<strong>" + marker.fsName + "</strong>" +
+                                  "</br>" + address +
+                                  "</br>(Address provided by Foursquare)" +
+                                  "</br><a href='http://foursquare.com/v/'" +
                                   marker.fsVenueID +
-                                  '" target="_blank"' +
-                                  '"><img src="img/foursquare150.png"></a>' +
-                                  '</div>');
+                                  " target='_blank'" +
+                                  "><img src='img/foursquare150.png'></a>" +
+                                  "</div>");
         } else {
             // Error fallback in case $.getJSON function response is empty
-            infowindow.setContent('<div>' +
-                                  '<strong>' + marker.name + '</strong>' +
-                                  '</br>' +
+            infowindow.setContent("<div>" +
+                                  "<strong>" + marker.name + "</strong>" +
+                                  "</br>" +
                                   "Foursquare data currently unavailable." +
-                                  '</div>');
+                                  "</div>");
         }
 
         infowindow.open(map, marker);
@@ -220,13 +220,13 @@ function initMap() {
         let self = this;
 
         // Constructor function creates an object of type Location when called
-        // with the 'new' keyword - this will be used to populate the locationList
+        // with the "new" keyword - this will be used to populate the locationList
         let Location = function (data) {
             this.name = data.name;
             this.position = data.position;
             this.type = data.type;
             this.venueID = data.venueID; // Foursquare venue ID
-            this.marker = ''; // gets assigned later when markers are created
+            this.marker = ""; // gets assigned later when markers are created
         };
 
         this.locationList = ko.observableArray([]);
@@ -268,7 +268,7 @@ function initMap() {
             });
 
             // Assign the newly created marker object to the marker property of
-            // the corresponding 'Location' object - This is what allows the filter
+            // the corresponding "Location" object - This is what allows the filter
             // function work on BOTH the list and the markers simultaneously, and
             // for the filter to act on both list and marker as well
             listItem.marker = marker;
@@ -276,9 +276,9 @@ function initMap() {
             bounds.extend(marker.position);
 
             // Create an onclick event to open an infowindow per marker.
-            marker.addListener('click', function() {
-                populateInfoWindow(this, largeInfowindow); // 'this' is the marker object
-                this.setIcon('http://maps.google.com/mapfiles/ms/micons/purple-dot.png')
+            marker.addListener("click", function() {
+                populateInfoWindow(this, largeInfowindow); // "this" is the marker object
+                this.setIcon("http://maps.google.com/mapfiles/ms/micons/purple-dot.png")
             });
         });
         // make sure markers are visible in window, lower zoom value if needed
@@ -294,19 +294,19 @@ function initMap() {
         // passes the corresponging marker as argument to populateInfoWindow
         this.itemClicked = function (clickedListItem) {
             populateInfoWindow(clickedListItem.marker, largeInfowindow);
-            // turns the marker purple when it's clicked
-            clickedListItem.marker.setIcon('http://maps.google.com/mapfiles/ms/micons/purple-dot.png');
+            // turns the marker purple when it"s clicked
+            clickedListItem.marker.setIcon("http://maps.google.com/mapfiles/ms/micons/purple-dot.png");
         }
 
-        // Create an observable array and pass it the 'filters' global array
+        // Create an observable array and pass it the "filters" global array
         this.filters = ko.observableArray(filters);
         // Set the initial filter to blank.
-        this.filter = ko.observable('');
+        this.filter = ko.observable("");
         // Create a computed observable which filters locations based on type.
         this.filteredLocations = ko.computed(function() {
             let filter = self.filter();
-            /* If filter is false OR is set to 'All' then return all of the
-            locations, otherwise return only the locations which have a 'type' value
+            /* If filter is false OR is set to "All" then return all of the
+            locations, otherwise return only the locations which have a "type" value
             which matches the filter value. */
             if (!filter || filter === "All") {
 
